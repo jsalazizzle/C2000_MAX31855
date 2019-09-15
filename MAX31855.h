78e70516a73982d5cb81299f6c6ed186f0124261
jsalazizzle <salazar.justin99@gmail.com>
2019-09-14 18:27:47 -0700

    licensing update

--- MAX31855.h
+++ MAX31855.h
@@ -1,3 +1,16 @@
+/*************************************************** 
+  This is a library for the Adafruit Thermocouple Sensor w/MAX31855K
+  Designed specifically to work with the Adafruit Thermocouple Sensor
+  ----> https://www.adafruit.com/products/269
+  These displays use SPI to communicate, 3 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
 #ifndef MAX31855_H
 #define MAX31855_H
 
